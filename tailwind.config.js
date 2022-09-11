0c955f86838e4ccdf9708458ade6a2281b33c411
Shehab Abdullah <shehab.abdullah12@gmail.comm>
2022-09-11 17:43:16 +0200

    install necessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      orange: '#FF7D1A',
+      paleOrange: '#FFEDE0',
+      white: '#fff',
+      black: '#000',
+    }
+  },
+  plugins: [],
+}
