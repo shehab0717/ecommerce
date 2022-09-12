6b0dcc8dfda5533a391112c5b2ff2f5b600345a4
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-12 16:39:59 +0200

    Design Sign up component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
+import SignUp from './components/SignUp/SignUp';
 import {products as productsData} from './dummyData';
 function App() {
   return (
     <>
-      <Navbar />
-      <Products data={productsData}/>
+      {/* <Navbar />
+      <Products data={productsData}/> */}
+      <SignUp />
     </>
   );
 }
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,7 +1,22 @@
 
 
-const SignUp = function():JSX.Element{
-    return <></>
+const SignUp = function (): JSX.Element {
+    return (
+        <>
+            <div className="text-center px-8 py-3 shadow-xl border rounded-md">
+                <h2 className="text-2xl font-semibold my-5">Sign up</h2>
+                <form>
+                    <input className="block w-full mb-4" type='text' placeholder='Full Name' />
+                    <input className="block w-full mb-4" type='text' placeholder='Email' />
+                    <input className="block w-full mb-4" type='text' placeholder='Password' />
+                    <input className="block w-full mb-4" type='text' placeholder='Confirm password' />
+                    <button className='w-full bg-green-500 border-0 text-white font-light py-2.5 hover:bg-green-400'>Create Account</button>
+                </form>
+                <p className="my-3 text-gray-400">By signing up, you agree to the <a href="#" className="underline">Terms of Service</a> and <a href="#" className="underline">Privacy Policy</a></p>
+            </div>
+            <p className="text-center mt-4 text-gray-500">Already have an account? <a href="" className="text-blue-500 underline">Log in</a></p>
+        </>
+    )
 }
 
 export default SignUp;
