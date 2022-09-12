444f0c3188bd68cf9247f35e1e1b1ddfe5387167
Shehab Abdullah <shehab.abdullah12@gmail.comm>
2022-09-12 14:19:42 +0200

    fix issue - tailwind with typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/**/*.{js,jsx,ts,tsx}"
-],
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    colors: {
-      orange: '#FF7D1A',
-      paleOrange: '#FFEDE0',
-      white: '#fff',
-      black: '#000',
+    extend: {
+      colors: {
+
+        orange: '#FF7D1A',
+        paleOrange: '#FFEDE0',
+        white: '#fff',
+        black: '#000',
+      }
     }
   },
   plugins: [],
