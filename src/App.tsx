83419ed90ba3113c4c4cc66a99345a54f456fe8a
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-13 02:15:10 +0200

    add generic style for selected image in the gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ProductDetails from './components/ProductDetails/ProductDetails';
 import Products from './components/Products/Products';
 import SignUp from './components/SignUp/SignUp';
 import {products as productsData} from './dummyData';
@@ -8,7 +9,7 @@ function App() {
     <>
       {/* <Navbar />
       <Products data={productsData}/> */}
-      <SignUp />
+      <ProductDetails />
     </>
   );
 }
