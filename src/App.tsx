725aec724790ae56ab4b127a0263830095fabe69
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-13 02:20:44 +0200

    add product details to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Navbar from './components/Navbar/Navbar';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 import Products from './components/Products/Products';
 import SignUp from './components/SignUp/SignUp';
-import {products as productsData} from './dummyData';
+import { products as productsData } from './dummyData';
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <Products data={productsData}/> */}
-      <ProductDetails />
+      <Navbar />
+      <div className="container mx-auto">
+        {/* <Products data={productsData}/> */}
+        <div className='mt-8'>
+          <ProductDetails />
+        </div>
+      </div>
     </>
   );
 }
