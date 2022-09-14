c6e5f0c0b51557d8006854978e468ede5f62224c
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-14 13:43:38 +0200

    export type for dispatcher and rootstate in the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,6 +4,8 @@ import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import { compose } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import allReducers from "./rootReducer";
+import { useDispatch } from 'react-redux'
+import { type } from "os";
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const composeEnhancers = compose;
@@ -11,4 +13,10 @@ const store = configureStore(
     { reducer: allReducers }
 )
 
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export default store;
