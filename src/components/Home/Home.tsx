3a6600564ab3281c11ad982724afcc58b29831cb
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-14 17:43:54 +0200

    home page automatically redirect to sign up if no user logged in

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,14 +2,18 @@ import Navbar from "../Navbar/Navbar"
 import Products from "../Products/Products";
 import { products as productsData } from "../../dummyData";
 import { useNavigate } from 'react-router-dom';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const Home = (): JSX.Element => {
 
+    const currentUser = useSelector(({ userReducer }: RootState) => userReducer.currentUser());
     const navigate = useNavigate();
-    useEffect(()=>{
-        navigate('/signup');
-    },[])
+    useEffect(() => {
+        if(!currentUser)
+            navigate('/signup');
+    }, [currentUser])
     return (
         <>
             <Navbar />
