348a3e31fffe848f468f9663764674c42a19db22
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-09-15 10:37:23 +0200

    countHandler button work

--- src/components/CountHandler/CountHandler.tsx
+++ src/components/CountHandler/CountHandler.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 
 import minusIcon from '../../assets/icons/icon-minus.svg';
 import plusIcon from '../../assets/icons/icon-plus.svg';
 
+type propsType = {
+    className?: string,
+    onChange: Function,
+}
 
-const CountHandler = ():JSX.Element => {
+const CountHandler = ({className, onChange}: propsType): JSX.Element => {
+    const [count, setCount] = useState(0);
+    function handleChange(increment: number) {
+        if (count + increment >= 0){
+            setCount(count + increment);
+            onChange(count + increment);
+        }
+    }
     return (
-        <div className='bg-gray-100 inline-block rounded-xl'>
-            <span className='p-3 px-5 cursor-pointer inline-block'><img className='inline' src={minusIcon}/></span>
-            <span className='mx-8 font-semibold'>0</span>
-            <span className='p-3 px-5 cursor-pointer inline-block'><img className='inline' src={plusIcon}/></span>
+        <div className={'bg-gray-100 inline-block rounded-xl' + className}>
+            <span className='p-3 px-5 cursor-pointer inline-block' onClick={() => handleChange(-1)}>
+                <img className='inline' src={minusIcon} />
+            </span>
+            <span className='mx-8 font-semibold'>{count}</span>
+            <span className='p-3 px-5 cursor-pointer inline-block' onClick={() => handleChange(+1)}>
+                <img className='inline' src={plusIcon} />
+            </span>
         </div>
     )
 }
